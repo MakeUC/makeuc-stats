f5d4eb918926b001fcf0b8e30fd337633d592e91
John Whiting <thejwhiting@gmail.com>
2022-05-31 20:18:52 -0400

    fix: Updated devpost link to 2021

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -4,7 +4,7 @@ export function Footer() {
   return <footer className="py-5 bg-primary-new">
     <div className="container">
       <p className="m-0 text-center text-white">
-        <a href="https://makeuc-2020.devpost.com/" className="btn-primary-new mt-2" style={{ margin: '0 auto' }}>Previous Projects</a>
+        <a href="https://makeuc-2021.devpost.com/" className="btn-primary-new mt-2" style={{ margin: '0 auto' }}>Previous Projects</a>
         <br />
         &copy; 2022 MakeUC
       </p>
